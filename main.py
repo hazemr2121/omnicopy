a6a0c46f8ce9df6c9027fa7f3966fd1752ac86d5
hazemr2121 <ha.ahmed8996@gmail.com>
2026-09-14 15:33:04 +0300

    Update .gitignore to include __pycache__ and refactor main.py for campaign management with SQLModel

--- main.py
+++ main.py
@@ -1,86 +1,102 @@
-from random import randint
+from fastapi import Depends, FastAPI, HTTPException, Request
+from datetime import datetime, timezone
+from typing import Annotated, Generic, TypeVar
 
-from fastapi import FastAPI, HTTPException, Request, Response
-from datetime import datetime
-from typing import Any
-app = FastAPI(root_path="/api/v1")
+from fastapi.concurrency import asynccontextmanager
+from pydantic import BaseModel
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+
+class Campaign(SQLModel, table=True):
+    campaign_id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    due_data: datetime | None = Field(default=None, index=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc),nullable=True,index=True)
+
+class CampaignCreate(SQLModel):
+    name: str 
+    due_data: datetime | None = None
+
+
+
+sqlite_file_name = "database.db"
+sql_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+
+engine = create_engine(sql_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    with Session(engine) as session:
+        if not session.exec(select(Campaign)).first():
+            session.add_all([
+                Campaign(name= "Summer Launch", due_data=datetime.now()),
+                Campaign(name= "Black Friday", due_data=datetime.now()),
+            ])
+            session.commit()
+    yield
+
+
+
+app = FastAPI(root_path="/api/v1", lifespan=lifespan)
 
 @app.get("/")
 async def root():
     return {"message":"hello world!"}
 
-data : Any = [
-    {
-        "campaign_id": 1,
-        "name": "Summer Lunch",
-        "due_date": datetime.now(),
-        "created_at": datetime.now()
-    },
-    {
-        "campaign_id": 2,
-        "name": "Black Friday",
-        "due_date": datetime.now(),
-        "created_at": datetime.now()
-    },
-]
+T = TypeVar("T")
+class Response(BaseModel, Generic[T]):
+    data: T 
 
-"""
-Compaigns
-- compaign_id
-- name
-- due_date
-- created_at
-"""
+@app.get("/campaigns", response_model=Response[list[Campaign]])
+async def read_campaigns(session: SessionDep):
+    data = session.exec(select(Campaign)).all()
+    return{"data": data}
 
-@app.get("/campaigns")
-async def read_campaigns():
-    return{"campaigns": data}
-
-@app.get("/campaigns/{id}")
-async def read_campaign(id: int):
-    for camp in data:
-        if camp.get("campaign_id") == id:
-            return {"campaign": camp}
-    raise HTTPException(status_code=404)
+@app.get("/campaigns/{id}", response_model=Response[Campaign])
+async def read_campaign(id: int, session: SessionDep):
+    data = session.get(Campaign,id)
+    if not data:
+        raise HTTPException(status_code=404)
+    return {"data":data}
 
 
-@app.post("/campaigns", status_code=201)
-async def create_campaign(body: dict[str, Any]):
-
-    new = {
-        "campaign_id": randint(100,1000),
-        "name": body.get("name"),
-        "due_date": body.get("due_date"),
-        "created_at": datetime.now()
-    }
-
-    data.append(new)
-
-    return{"campaign": new}
+@app.post("/campaigns", status_code=201, response_model=Response[Campaign])
+async def create_campaign(campaign: CampaignCreate, session: SessionDep):
+    db_campaign = Campaign.model_validate(campaign)
+    session.add(db_campaign)
+    session.commit()
+    session.refresh(db_campaign)
+    return{"data": db_campaign}
 
 
-@app.put("campaigns/{id}")
-async def update_campaign(id: int,body: dict[str, Any]):
-    for index, camp in enumerate(data):
-        if camp.get("campaign_id") == id:
-
-            updated = {
-                "campaign_id": id,
-                "name": body.get("name"),
-                "due_date": body.get("due_date"),
-                "created_at": camp.get("created_at")
-            }
-            data[index] = updated
-            return {"campaign": updated}
-
-    raise HTTPException(status_code=404)
+@app.put("campaigns/{id}", response_model=Response[Campaign])
+async def update_campaign(id: int,campaign: CampaignCreate, session: SessionDep):
+    data = session.get(campaign, id)
+    if not data:
+        raise HTTPException(status_code=404)
+    data.name = campaign.name
+    data.due_date = campaign.due_date
+    session.add(data)
+    session.commit()
+    session.refresh(data)
+    return {"data": data}
 
 
-@app.delete("campaigns/{id}")
-async def update_campaign(id: int):
-    for index, camp in enumerate(data):
-        if camp.get("campaign_id") == id:
-
-            data.pop(index)            
-            return Response(status_code=204)
-    raise HTTPException(status_code=404)
+@app.delete("campaigns/{id}", status_code=204)
+async def update_campaign(id: int, session: SessionDep):
+    data = session.get(Campaign, id)
+    if not data:
+        raise HTTPException(status_code=404)
+    session.delete(data)
+    session.commit()
